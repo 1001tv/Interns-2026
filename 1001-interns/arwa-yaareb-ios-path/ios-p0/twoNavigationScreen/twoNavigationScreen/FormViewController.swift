52f38033901e053174bc49c60b9d980b6c5355e1
arwayaareb <arwa.yaareb@1001.tv>
2026-09-20 14:35:23 +0300

    first actual correct submit of my first ios simple project

---
+++ 1001-interns/arwa-yaareb-ios-path/ios-p0/twoNavigationScreen/twoNavigationScreen/FormViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FormViewController.swift
+//  twoNavigationScreen
+//
+//  Created by Arwa Yaareb on 19/09/2026.
+//
+
+import UIKit
+
+class FormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
---
+++ 1001-interns/arwa-yaareb-ios-path/ios-p0/twoNavigationScreen/twoNavigationScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twoNavigationScreen
+//
+//  Created by Arwa Yaareb on 19/09/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
