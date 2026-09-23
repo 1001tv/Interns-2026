eb27902ea4b93fc0070c60b88e75c70306564e72
arwayaareb <arwa.yaareb@1001.tv>
2026-09-23 12:20:40 +0300

    learning if and guard statements in swift

--- 1001-interns/arwa-yaareb-ios-path/ios-p0/swift-self-learning/swift-playground-self-learning.swift
+++ 1001-interns/arwa-yaareb-ios-path/ios-p0/swift-self-learning/swift-playground-self-learning.swift
@@ -1,19 +1,38 @@
 import Playgrounds
 
 #Playground {
-    // let maximumNumberOfLoginAttempts = 10
-    var wlecomeMessage = "Hello"
+//    let maximumNumberOfLoginAttempts = 10
+//    var wlecomeMessage = "Hello"
+//    
+//    var environment = "development"
+//    let maximumNumberOfLoginAttempts: Int // in this case the maximumNumberOfLoginAttempts does not have a value
+//    
+//    
+//     if we did some checking:
+//    if environment == "development" {
+//        maximumNumberOfLoginAttempts = 100
+//    } else {
+//        maximumNumberOfLoginAttempts = 10
+//    }
+//    
+//    type annotation
+//
+    // if we had a button and the button text was named nameTextField then we take te text of it and save it to a variable and check if it does exist then print it. if does not, then say text field is nil.
     
-    var environment = "development"
-    let maximumNumberOfLoginAttempts: Int // in this case the maximumNumberOfLoginAttempts does not have a value
-    
-    
-    // if we did some checking:
-    if environment == "development" {
-        maximumNumberOfLoginAttempts = 100
+    if let actualName = nameTextField.text {
+        print("user entered: \(actualName)")
     } else {
-        maximumNumberOfLoginAttempts = 10
+        print("text field is nil")
     }
     
-    //type annotation
+    // ORR we can use the guard statement, it is faster nad done in one line.
+    // we call this early exit.
+    // here it exists early if the email is missigg or its empty usign the isEmpty. if not it will just return its value.
+    
+    // this one is nore used than the if condition and it is faster to implement
+    
+    guard let actualEmail = nameTextField.text, !actualEmail.isEmpty else return
+        
+    
+    
 }
